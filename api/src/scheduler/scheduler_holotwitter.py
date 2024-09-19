535db62c8b6354203a0fcb1c504d6f3e691c339f
sakura7304moti <sakura7304moti@gmail.com>
2024-09-19 22:31:21 +0900

    スケジューラー作成

---
+++ api/src/scheduler/scheduler_holotwitter.py
@@ -0,0 +1,14 @@
+import os
+import sys
+api_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+print(api_path)
+sys.path.append(api_path)
+
+from src.route.service import holotwitter_service as service
+from src.route.service.module.utils import const
+
+@const.line_handler
+def main():
+    service.scraping()
+
+main()
