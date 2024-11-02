56d9a6a001f3a9afed60a9332aa8159c3a2a8577
sakura7304moti <sakura7304moti@gmail.com>
2024-11-02 20:09:41 +0900

    ページデプロイしたときにリロードするようにした。何か問題があったらこの箇所を削除しよう

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -8,4 +8,10 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'App',
 });
+window.addEventListener('error', (event) => {
+  if (event.message.includes('Failed to fetch dynamically')) {
+    // エラーをキャッチしたら自動リロード
+    window.location.reload();
+  }
+});
 </script>
